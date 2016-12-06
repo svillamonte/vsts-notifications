d883603ba0953493d5485103d8775359d86c3d7a
Sebastián Villamonte <walden2av@gmail.com>
2016-12-06 21:03:35 +1300

    Added production environment settings and modified the configuration builder so it accepts changes on the environment value from the command line.

--- src/VstsNotifications.Webhooks/Program.cs
+++ src/VstsNotifications.Webhooks/Program.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
@@ -14,8 +10,8 @@ namespace VstsNotifications.Webhooks
         public static void Main(string[] args)
         {
             var config = new ConfigurationBuilder()
-                .AddCommandLine(args)
                 .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                .AddCommandLine(args)
                 .Build();
             
             var host = new WebHostBuilder()
