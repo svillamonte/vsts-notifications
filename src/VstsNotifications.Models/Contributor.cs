093d85954def86fe2f1fad92ba0626353c428413
Sebastián Villamonte <walden2av@gmail.com>
2016-12-06 21:05:10 +1300

    Got rid of unnecessary comment.

--- src/VstsNotifications.Models/Contributor.cs
+++ src/VstsNotifications.Models/Contributor.cs
@@ -7,17 +7,3 @@ namespace VstsNotifications.Models
         public string SlackUserId { get; set; }
     }
 }
-
-/*
-kieran U045UQ2GZ
-rdsimes U2G98VBRB
-scoff U02BEAMC3
-seba U04ANHY0K
-alan U02BF34V2
-amanda U034VF85L
-dbdave U02F6566Q
-justin U02BMDQHG
-rygyver U21L8SBRQ
-sam U0FP5UP98
-will U025P066G
-*/
